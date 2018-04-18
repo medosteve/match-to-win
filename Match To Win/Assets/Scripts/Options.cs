d8a7f53a7968243498890de6ceb333066d2a9fd9
medosteve <medosteve7@gmail.com>
2018-04-18 23:07:56 +0200

    the final project

---
+++ Match To Win/Assets/Scripts/Options.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource myMusic;
+ 
+    void Update () {
+        if (myMusic != null)  {
+            myMusic.volume = Volume.value;
+           
+        }
+    }
+}
